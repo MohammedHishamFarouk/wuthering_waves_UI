c116f6c59b2997daef4862161849673cb1b09028
mohammed hisham <mohamedhis36@gmail.com>
2025-07-30 11:52:53 +0300

    added an assets.dart file to organize the use of assets

---
+++ lib/assets.dart
@@ -0,0 +1,18 @@
+class Assets {
+  static const List<String> statIcons = [
+    'assets/icons/stats_icons/HP.png',
+    'assets/icons/stats_icons/ATK.png',
+    'assets/icons/stats_icons/DEF.png',
+    'assets/icons/stats_icons/ER.png',
+    'assets/icons/stats_icons/CR.png',
+    'assets/icons/stats_icons/CD.png',
+  ];
+  static const List<String> statNames = [
+    'HP',
+    'ATK',
+    'DEF',
+    'Energy Regen',
+    'Crit. Rate',
+    'Crit. DMG',
+  ];
+}
--- lib/screens/character_view_screen.dart
+++ lib/screens/character_view_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wuthering_waves_ui_clone/character_stats.dart';
 import 'package:wuthering_waves_ui_clone/components/character_icon.dart';
-import 'package:wuthering_waves_ui_clone/components/stats_box.dart';
 
 class CharacterViewScreen extends StatelessWidget {
   const CharacterViewScreen({super.key});
@@ -20,58 +20,37 @@ class CharacterViewScreen extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.only(
-              left: 60,
+              left: 46,
               right: 60,
               bottom: 64,
-              top: 48,
+              top: 40,
             ),
             child: Row(
               children: [
                 const Column(),
-                Column(
-                  children: [
-                    const StatsBox(),
-                    const Spacer(),
-                    FilledButton(
-                      onPressed: () {},
-                      style: FilledButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        maximumSize: const Size(402, 58),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        padding: EdgeInsets.zero,
-                      ),
-                      child: const Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Image(image: AssetImage('assets/left.png')),
-                          Spacer(),
-                          Text(
-                            'Level Up',
-                            style: TextStyle(
-                              color: Colors.black54,
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Spacer(flex: 2),
-                          Image(image: AssetImage('assets/right.png')),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                const CharacterStats(),
+                const Spacer(),
                 const Column(),
                 const Spacer(),
                 SizedBox(
                   width: 80,
-                  child: ListView.separated(
-                    shrinkWrap: true,
-                    itemCount: 9,
-                    separatorBuilder: (context, index) =>
-                        const SizedBox(height: 18),
-                    itemBuilder: (context, index) => charactersOwned()[index],
+                  child: Column(
+                    spacing: 68,
+                    children: [
+                      const Image(
+                        image: AssetImage('assets/icons/exitIcon.png'),
+                      ),
+                      Expanded(
+                        child: ListView.separated(
+                          shrinkWrap: true,
+                          itemCount: charactersOwned().length,
+                          separatorBuilder: (context, index) =>
+                              const SizedBox(height: 18),
+                          itemBuilder: (context, index) =>
+                              charactersOwned()[index],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
